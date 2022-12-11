65dde5e12dfab71bb44c5d29290c2e66cd4383db
jennifer-mcallister <jennifer_mca@hotmail.com>
2022-12-11 23:46:05 +0100

    cleaned up code

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -27,22 +27,25 @@ const movies :{} = {Search:[
     }
 ]};
 
-
-
 beforeEach (()=> {
   cy.visit('http://localhost:1234');
   cy.intercept("GET", "http://omdbapi.com/*", movies).as("moviesCall");
 });
 
-// bättre namn i describtion
+
 describe('userexperience flow', () => {
+  
   it('should be able to type', () => {
+
     cy.get("input").type(searchText).should("have.value", searchText);
+
   });
 
   it('should be able to click', () => {
+
     cy.get("input").type(searchText).should("have.value", searchText);
     cy.get("button").click(); 
+
   });
 
   it('should be able to search and get data', () => {
@@ -53,27 +56,30 @@ describe('userexperience flow', () => {
     cy.get("div#movie-container > div.movie").should("have.length", 3);
     cy.get("div.movie:first > h3").contains(movieTitle);
     cy.get("div.movie:first").find("img").should("have.attr", "src", pathToPoster);
-  });
 
+  });
 });
 
 
 describe("API call", ()=> {
 
-  it("should get fake API", ()=> {
+  it("should get fake requested url", ()=> {
+
     cy.intercept("GET", "http://omdbapi.com/*", movies).as("moviesCall");
     cy.get("input").type(searchText).should("have.value", searchText);
     cy.get("button").click();
     cy.wait("@moviesCall").its("request.url").should("contain", "=hello");
+
   });
 
   it("should get error", ()=> {
+
     cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=", {forceNetworkError: true}).as("err");
-    
-    cy.visit('http://localhost:1234');
+
     cy.get("button").click();
     cy.wait("@err").should("have.property", "error");
     cy.get("div#movie-container > p").contains("Inga sökresultat att visa");
+    
   });
 });
 
